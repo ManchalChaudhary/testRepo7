1b6fb399bb77596837cdbf0f4670907946b6832c
manchal <manchalc@gmail.com>
2025-02-01 11:17:25 -0600

    my second commit

--- src/matrixCalx/Matrix.java
+++ src/matrixCalx/Matrix.java
@@ -4,7 +4,7 @@ public class Matrix {
 	
 	public static void main(String[] args) {
 		
-		int a = 2;
+		int a = 4;
 		//break point, debug mode: step into, step over, step return
 		for(int i= 1; i<=a; i++) {
 			
@@ -13,6 +13,7 @@ public class Matrix {
 				System.out.print(i*j + "\t" );
 			}
 			System.out.println();
+			
 		}
 	}
 
