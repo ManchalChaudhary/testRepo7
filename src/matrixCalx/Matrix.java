85874428c85b30df76747cace63e69d76732a286
manchal <manchalc@gmail.com>
2025-02-01 11:12:17 -0600

    first commit

---
+++ src/matrixCalx/Matrix.java
@@ -0,0 +1,19 @@
+package matrixCalx;
+
+public class Matrix {
+	
+	public static void main(String[] args) {
+		
+		int a = 2;
+		//break point, debug mode: step into, step over, step return
+		for(int i= 1; i<=a; i++) {
+			
+			for(int j=1; j<=a; j++) {
+				
+				System.out.print(i*j + "\t" );
+			}
+			System.out.println();
+		}
+	}
+
+}
